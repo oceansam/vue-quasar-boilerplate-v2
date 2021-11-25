30896fe7bea65102b810fb63733c019696e85cbe
Sam <oceansam101@gmail.com>
2021-11-24 22:08:04 -0500

    Setup Client

---
+++ src/components/SampleComponent.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="flex">
+    <q-img
+      src="https://freediscordstickers.com/wp-content/uploads/pepepls.png"
+      width="120px"
+      height="120px"
+    />
+    <div>
+      <a href="https://github.com/oceansam">Github</a>
+    </div>
+  </div>
+</template>
---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <div class="flex justify-center">
+      <h1>Vue + Quasar Boilerplate</h1>
+    </div>
+    <div class="flex justify-center">
+      <div>
+        <q-input v-model="inputField" label="Example Input" />
+        <div>Your Input is: {{ inputField }}</div>
+      </div>
+    </div>
+    <div class="flex justify-center q-mt-xl">
+      <sample-component />
+    </div>
+  </div>
+</template>
+
+<script>
+import { defineComponent, ref } from "vue";
+// Components
+import SampleComponent from "src/components/SampleComponent.vue";
+export default defineComponent({
+  name: "MainLayout",
+  components: {
+    SampleComponent,
+  },
+  setup() {
+    const inputField = ref();
+
+    return {
+      inputField,
+    };
+  },
+});
+</script>
